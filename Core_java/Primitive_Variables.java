d4f5a1a8ef3027d8e61c047a1470d06c2db9bffa
I-AM-IRONMAN-10 <23aids044@act.edu.in>
2026-03-31 11:30:00 +0530

    Primitive data types

--- Core_java/Primitive_Variables.java
+++ Core_java/Primitive_Variables.java
@@ -1,5 +1,17 @@
+
+
 public class Primitive_Variables {
     public static void main(String[] args) {
-        
+        int myNum = 5;               // Integer (whole number)
+        double myDoubleNum = 5.99;   // Floating point number
+        char myLetter = 'D';         // Character
+        boolean myBool = true;       // Boolean
+        String myText = "Hello";     // String (text)
+        byte myByte = 100;          // Byte (whole number from -128 to 127)
+        short myShort = 5000;       // Short (whole number from -32,768 to 32,767)
+        long myLong = 15000000000L; // Long (whole number from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807)
+        System.out.println(myByte+myNum+myDoubleNum+myShort+myLong);
+        System.out.println(myLetter);
+        System.out.println(myText);
     }
 }
